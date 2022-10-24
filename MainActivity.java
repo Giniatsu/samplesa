6462743e648570d6c668b313c32b97604ac784c4
Giniatsu <110588543+Giniatsu@users.noreply.github.com>
2022-10-24 22:12:59 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,72 +1,72 @@
 package com.example.elective2_garcia;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth _auth;
-    private FirebaseFirestore db;
-    ListView coursesLV;
-    ArrayList<DataModal> dataModalArrayList;
+public class CoursesLVAdapter extends ArrayAdapter<DataModal> {
+    public CoursesLVAdapter(@NonNull Context context, ArrayList<DataModal> dataModalArrayList) {
+        super(context, 0, dataModalArrayList);
+    }
 
+    @NonNull
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        View listitemView = convertView;
+        if (listitemView == null){
+            listitemView = LayoutInflater.from(getContext()).inflate(R.layout.items_cardview, parent, false);
+        }
 
-        coursesLV = findViewById(R.id.idLVCourses);
-        dataModalArrayList = new ArrayList<>();
+        DataModal dataModal = getItem(position);
 
-        _auth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
+        TextView pdname = listitemView.findViewById(R.id.pname);
+        TextView pddesc = listitemView.findViewById(R.id.pdesc);
+        TextView pdprice = listitemView.findViewById(R.id.pprice);
+        ImageView pdimage = listitemView.findViewById(R.id.imgUrl);
 
-        loadDatainListview();
+        pdname.setText(dataModal.getName());
+        pddesc.setText(dataModal.getDescription());
+        pdprice.setText(dataModal.getPrice());
+
+        Picasso.get().load(dataModal.getImgUrl().get(0)).into(pdimage);
+
+        listitemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Item clicked is : " + dataModal.getName(), Toast.LENGTH_SHORT).show();
+                String productname = pdname.getText().toString();
+                String productdesc = pddesc.getText().toString();
+                String productprice = pdprice.getText().toString();
+                String p1 = dataModal.getImgUrl().get(0);
+                Log.d("T10000", p1);
+                String p2 = dataModal.getImgUrl().get(1);
+                Intent intent = new Intent(getContext(), ProductViewActivity2.class);
+                intent.putExtra("name", productname);
+                intent.putExtra("desc", productdesc);
+                intent.putExtra("price", productprice);
+                intent.putExtra("image1", p1);
+                intent.putExtra("image2", p2);
+                getContext().startActivity(intent);
+            }
+        });
+
+        return listitemView;
     }
-
-    private void loadDatainListview(){
-        db.collection("items").get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        if(!queryDocumentSnapshots.isEmpty()){
-                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
-                            for(DocumentSnapshot d: list){
-                                DataModal dataModal = d.toObject(DataModal.class);
-                                dataModalArrayList.add(dataModal);
-                            }
-                            CoursesLVAdapter adapter = new CoursesLVAdapter(MainActivity.this, dataModalArrayList);
-                            coursesLV.setAdapter(adapter);
-                        }else{
-                            Toast.makeText(MainActivity.this, "No Data found in Database", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Failed to load Data.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-    }
-
-    public void logout(View v){
-        _auth.signOut();
-        finish();
-    }
-
 }
